d12c0a62936936641f5bc3def4620e900375a437
Deniz A. Durmaz <denizalpd@ogr.iu.edu.tr>
2019-08-23 14:49:40 +0300

    getMessages returns [Message] instead of just text field

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 module Main where
 
@@ -6,19 +7,20 @@ import           Network.HTTP.Client      (newManager)
 import           Network.HTTP.Client.TLS  (tlsManagerSettings)
 import           Web.Telegram.API.Bot
 import Data.Maybe
+import Servant.Client.Core.Internal.Request
 
 token :: Token
-token = Token "token"
+token = Token "<token>"
 
+getMessages ::  IO (Either ServantError [Message]) 
 getMessages = do
   manager <- newManager tlsManagerSettings
   r1 <- getUpdates token Nothing Nothing Nothing manager
-  return $ mapMaybe text . mapMaybe message . result <$> r1
+  return $ mapMaybe message . result <$> r1
 
 main :: IO ()
 main = do
   print $ "wow"
-
 --  void $ runTelegramClient token manager $ do
 --    sendMessageM (SendMessageRequest (ChatChannel "-1001330413002") "" Nothing Nothing Nothing Nothing Nothing)
 
