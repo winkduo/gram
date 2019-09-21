60d31ab378e479d4296b6be77b3ba9920b177389
Yigit Ozkavci <yigitozkavci8@gmail.com>
2019-09-21 15:30:15 +0100

    Fix formatting

--- nix/trivial.nix
+++ nix/trivial.nix
@@ -1,24 +1,18 @@
 {
   network.description = "Web server";
-
-	gramserver =
-    { config, pkgs, ... }:
-    let
-      gram = import ../default.nix;
-    in
-			{ networking.hostName = "gram";
-
-				networking.firewall.allowedTCPPorts = [ 22 80 ];
-				environment.systemPackages = [ gram ];
-
-				systemd.services.gram =
-					{ description = "gram Webserver";
-						wantedBy = [ "multi-user.target" ];
-						after = [ "network.target" ];
-						serviceConfig =
-							{ ExecStart = "${gram}/bin/gram";
-                EnvironmentFile = "${builtins.path { name = "dotenv"; path = ../.env; }}";
-							};
-					};
-			};
+  gram = import ../default.nix;
+  gramserver = { config, pkgs, ... }: {
+    networking.hostName = "gram";
+    networking.firewall.allowedTCPPorts = [ 22 80 ];
+    environment.systemPackages = [ gram ];
+    systemd.services.gram = {
+      description = "gram Webserver";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig = {
+        ExecStart = "${gram}/bin/gram";
+        EnvironmentFile = "${builtins.path { name = "dotenv"; path = ../.env; }}";
+      };
+    };
+  };
 }
